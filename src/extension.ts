9f0055b6f8bb1476b220459860401f5b986774b1
1inguini <9647142@gmail.com>
2021-08-20 10:16:39 +0900

    hide even when focus change, not only mouse click

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
         let path = vscode.window.activeTextEditor.document.fileName;
         let pathIsFile = path.includes(".") || path.includes("\\") || path.includes("/");
 
-        if (selection.kind != TextEditorSelectionChangeKind.Mouse // selection was not from a click
+        if (/* selection.kind != TextEditorSelectionChangeKind.Mouse // selection was not from a click
             || selection.selections.length != 1                   // no selections or multiselections
-            || selection.selections.find(a=>a.isEmpty) == null    // multiselections
+            || */ selection.selections.find(a=>a.isEmpty) == null    // multiselections
             || !pathIsFile) {                                     // The debug window editor
             return;
         } else {
